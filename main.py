ab60fba7614d457856bcd6dc86563df40be1b332
Kirill <kosyanip@gmail.com>
2024-08-29 23:01:17 +0300

    added selenium to project

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from time import sleep
+from selenium.webdriver import Chrome, ChromeOptions
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+options = ChromeOptions()
+options.add_argument("--headless")
+driver = Chrome(options=options)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+driver.get("https://example.com")
+sleep(5)
